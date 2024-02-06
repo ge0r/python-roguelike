3dc32e93283491a496b6f9ea76d4289f5abdeebd
George Gkioulis <george.gkioulis@gmail.com>
2024-02-06 22:30:02 +0100

    Add equipment

--- entity.py
+++ entity.py
@@ -9,6 +9,8 @@ from render_order import RenderOrder
 if TYPE_CHECKING:
     from components.ai import BaseAI
     from components.consumable import Consumable
+    from components.equipment import Equipment
+    from components.equipable import Equipable
     from components.fighter import Fighter
     from components.inventory import Inventory
     from components.level import Level
@@ -99,6 +101,7 @@ class Actor(Entity):
         color: Tuple[int, int, int] = (255, 255, 255),
         name: str = "<Unnamed>",
         ai_cls: Type[BaseAI],
+        equipment: Equipment,
         fighter: Fighter,
         inventory: Inventory,
         level: Level,
@@ -114,6 +117,9 @@ class Actor(Entity):
         )
 
         self.ai: Optional[BaseAI] = ai_cls(self)
+
+        self.equipment: Equipment = equipment
+        self.equipment.parent = self
 
         self.fighter = fighter
         self.fighter.parent = self
@@ -138,7 +144,8 @@ class Item(Entity):
         char: str = "?",
         color: Tuple[int, int, int] = (255, 255, 255),
         name: str = "<Unnamed>",
-        consumable: Consumable,
+        consumable: Optional[Consumable] = None,
+        equipable: Optional[Equipable] = None,
     ):
 
         super().__init__(
@@ -152,4 +159,12 @@ class Item(Entity):
         )
 
         self.consumable = consumable
-        self.consumable.parent = self
+
+        if self.consumable:
+            self.consumable.parent = self
+
+
+        self.equipable = equipable
+
+        if self.equipable:
+            self.equipable.parent = self
